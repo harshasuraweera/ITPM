150fda0436b2fe76495b0d6f189802e1fc649f35
Harsha Manoj Suraweera <harshamanoj912@gmail.com>
2021-05-09 19:49:48 +0530

    assign session rooms completed upto 75%

--- ITPM/src/it19208718/AssignSessionRoom.java
+++ ITPM/src/it19208718/AssignSessionRoom.java
@@ -7,21 +7,39 @@ import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
 
+import javax.swing.DefaultComboBoxModel;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+
+import database.DBConnect;
+
 import javax.swing.JLabel;
 import javax.swing.SwingConstants;
 import javax.swing.JComboBox;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
+import javax.swing.ListModel;
+import javax.swing.JList;
+import javax.swing.JOptionPane;
+import javax.swing.UIManager;
+import java.awt.SystemColor;
+import javax.swing.JTextPane;
 
 public class AssignSessionRoom extends JFrame {
 
 	private JPanel contentPane;
+	private ArrayList<String> selectedSessionsArrayList =  new ArrayList<String>();  
+	private ArrayList<String> selectedRoomssArrayList =  new ArrayList<String>(); 
+	private ArrayList<String> selectedAllSessions = new ArrayList<String>();
 	private JTextField ifTheSelectedSessionConsectiveShowTheRelatedOneHere;
 
 	/**
@@ -110,14 +128,18 @@ public class AssignSessionRoom extends JFrame {
 		btnManageLocation.setBounds(940, 10, 302, 50);
 		panel.add(btnManageLocation);
 		
+		JTextArea selectedSessions = new JTextArea();
+		selectedSessions.setBounds(291, 456, 806, 122);
+		contentPane.add(selectedSessions);
+		
 		JLabel addNewLocationLabel = new JLabel("Assign Session Rooms");
 		addNewLocationLabel.setHorizontalAlignment(SwingConstants.LEFT);
 		addNewLocationLabel.setFont(new Font("Kristen ITC", Font.BOLD, 30));
 		addNewLocationLabel.setBounds(457, 122, 345, 44);
 		contentPane.add(addNewLocationLabel);
 		
-		JComboBox<Object> allTheSessionsDropDownList = new JComboBox<Object>(new Object[]{});
-		//roomTypeFilter.setSelectedIndex(0);
+		JComboBox<Object> allTheSessionsDropDownList = new JComboBox<Object>(getSessionsList());
+		allTheSessionsDropDownList.setSelectedIndex(0);
 		allTheSessionsDropDownList.setFont(new Font("Kristen ITC", Font.PLAIN, 18));
 		allTheSessionsDropDownList.setBackground(Color.WHITE);
 		allTheSessionsDropDownList.setBounds(291, 199, 818, 36);
@@ -139,7 +161,7 @@ public class AssignSessionRoom extends JFrame {
 		contentPane.add(lblSelectRoom);
 		
 		JComboBox<Object> allTheAvailableRoomList = new JComboBox<Object>(new Object[]{});
-		//roomTypeFilter_1_1.setSelectedIndex(0);
+		//allTheAvailableRoomList.setSelectedIndex(0);
 		allTheAvailableRoomList.setFont(new Font("Kristen ITC", Font.PLAIN, 18));
 		allTheAvailableRoomList.setBackground(Color.WHITE);
 		allTheAvailableRoomList.setBounds(291, 372, 818, 36);
@@ -150,22 +172,24 @@ public class AssignSessionRoom extends JFrame {
 		lblSelectedSessions.setBounds(116, 456, 165, 46);
 		contentPane.add(lblSelectedSessions);
 		
-		JTextArea selectedSessionListArray = new JTextArea();
-		selectedSessionListArray.setBackground(Color.WHITE);
-		selectedSessionListArray.setBounds(291, 456, 818, 112);
-		contentPane.add(selectedSessionListArray);
-		
 		JButton assignSessionBtn = new JButton("Assign");
-		assignSessionBtn.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-			}
-		});
 		assignSessionBtn.setFont(new Font("Kristen ITC", Font.PLAIN, 18));
 		assignSessionBtn.setFocusable(false);
 		assignSessionBtn.setBounds(116, 608, 120, 50);
 		contentPane.add(assignSessionBtn);
 		
+		
+		//clear selected items
 		JButton clearSelectedSessionArrayBtn = new JButton("Clear");
+		clearSelectedSessionArrayBtn.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				selectedSessions.setText("");
+				selectedSessionsArrayList.clear();
+				selectedRoomssArrayList.clear();
+				selectedAllSessions.clear();
+				
+			}
+		});
 		clearSelectedSessionArrayBtn.setFont(new Font("Kristen ITC", Font.PLAIN, 18));
 		clearSelectedSessionArrayBtn.setFocusable(false);
 		clearSelectedSessionArrayBtn.setBounds(288, 608, 120, 50);
@@ -178,5 +202,273 @@ public class AssignSessionRoom extends JFrame {
 		ifTheSelectedSessionConsectiveShowTheRelatedOneHere.setColumns(10);
 		ifTheSelectedSessionConsectiveShowTheRelatedOneHere.setBounds(291, 282, 818, 36);
 		contentPane.add(ifTheSelectedSessionConsectiveShowTheRelatedOneHere);
+		
+		
+		
+		
+		
+		
+		//on select the session, according to the tag,  load locations
+		allTheSessionsDropDownList.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				
+				String selectedSession = String.valueOf(allTheSessionsDropDownList.getSelectedItem());
+				
+				if(selectedSession.endsWith("Tutorial")) { //is the selected session a Tutorial, load lecture halls to the room dropdown list
+					
+					ifTheSelectedSessionConsectiveShowTheRelatedOneHere.setText("Tutorial");
+					allTheAvailableRoomList.setModel(new DefaultComboBoxModel<Object>(getRoomNames("Lecture Hall")));
+					
+					
+				}else if(selectedSession.endsWith("Lab")) { //is the selected session a Lab, load laboratories to the room dropdown list
+					
+					ifTheSelectedSessionConsectiveShowTheRelatedOneHere.setText("Lab");
+					allTheAvailableRoomList.setModel(new DefaultComboBoxModel<Object>(getRoomNames("Laboratory")));
+					
+				}
+				
+				
+				
+				
+			}
+		});
+		
+		
+		
+		//after select the room, take the complete session ID and the roomID to diffrent array
+		allTheAvailableRoomList.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				String selectedSessionName = String.valueOf(allTheSessionsDropDownList.getSelectedItem());
+				String selectedRoomName = String.valueOf(allTheAvailableRoomList.getSelectedItem());
+				
+				selectedSessionsArrayList.add(selectedSessionName);
+				selectedRoomssArrayList.add(selectedRoomName);
+				
+				
+				
+				//load these to selected sessions textbox
+				selectedAllSessions.add(selectedSessionName + " At " + selectedRoomName );
+
+				selectedSessions.setText("");
+				for(String s : selectedAllSessions){
+					selectedSessions.append(s + System.getProperty("line.separator"));
+				}
+				
+				allTheSessionsDropDownList.setSelectedIndex(0);
+				
+				allTheAvailableRoomList.setSelectedIndex(0);
+			}
+		});
+		
+		
+		//insert into session table on button clicked + make the sessionAssignedStatus as assigned
+		assignSessionBtn.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+				int count = 0;
+				
+				int dialogButton = JOptionPane.YES_NO_OPTION;
+				int dialogResult = JOptionPane.showConfirmDialog (null, "Operation cannot be undone! Do you want to continue?","Warning",dialogButton);
+				
+				if(dialogResult == JOptionPane.YES_OPTION){
+					
+					for(int i=0; i<selectedSessionsArrayList.size(); i++) {
+						
+						String sessionId = String.valueOf(selectedSessionsArrayList.get(i).toString().charAt(2)) + String.valueOf( selectedSessionsArrayList.get(i).toString().charAt(3) );
+
+						String locationId = String.valueOf(selectedRoomssArrayList.get(i).toString().charAt(2)) + String.valueOf( selectedRoomssArrayList.get(i).toString().charAt(3) );
+						
+						//make the session as a room assigned session
+						markAsRoomAssigned(sessionId);
+						
+						//reload sessions list
+						allTheSessionsDropDownList.setModel(new DefaultComboBoxModel(getSessionsList()));
+						
+						boolean isSuccess = insertIntoSessionRoomTable(sessionId, locationId);
+						if(isSuccess) {
+							count++;
+						}
+						
+					}
+					
+					if(count == selectedSessionsArrayList.size()) {
+						
+					}
+					
+					selectedSessions.setText("");
+					selectedSessionsArrayList.clear();
+					selectedRoomssArrayList.clear();
+					selectedAllSessions.clear();
+					
+				}else {
+					
+						
+				}
+					
+			}
+		});
+		
+		
 	}
+	
+	
+	//load sessions to the dropdown list
+	private String [] getSessionsList () {
+		
+		Connection conn = DBConnect.getConnection();
+		
+		String[] sessionListArray = null;
+		List<String> list = new ArrayList<>();
+		String singleRoom;
+		String AIID;
+		
+		try {
+			String sql = "SELECT * from Sessions WHERE roomAssignedStatus = 'not-assigned' ";
+			Statement st = conn.createStatement();
+			ResultSet rs = st.executeQuery(sql);
+			
+			list.add("Select a available session on here");
+			
+			while(rs.next()) {
+				
+				if( rs.getString("id").equals("1") || rs.getString("id").equals("2") || rs.getString("id").equals("3")|| rs.getString("id").equals("4") || rs.getString("id").equals("5") 
+						|| rs.getString("id").equals("6") || rs.getString("id").equals("7") || rs.getString("id").equals("8") || rs.getString("id").equals("9") ) {
+					
+					AIID = "0" + rs.getString("id");
+					
+				}else {
+					AIID = rs.getString("id");
+				}
+				
+				singleRoom = "ID"+ AIID + " | " + rs.getString("subjectName") + " (" + rs.getString("subjectCode") + ") for " + rs.getString("groupId") + " by " + rs.getString("lecturer1") + " and " + rs.getString("lecturer2") + " | "  + rs.getString("tag");
+				list.add(singleRoom);
+			}
+			sessionListArray = list.toArray(new String[0]);
+			
+		}catch (Exception e) {
+			
+		}
+
+		
+		return sessionListArray;
+	}
+	
+	
+	
+	//get all the rooms available to a list
+	private String [] getRoomNames(String roomType) {
+		
+		Connection conn = DBConnect.getConnection();
+		
+		String[] roomListArray = null;
+		List<String> list = new ArrayList<>();
+		String singleRoom;
+		
+		String AIID;
+		
+		
+		try {
+			String sql = "SELECT * from Locations WHERE roomType = '"+roomType+"' ";
+			Statement st = conn.createStatement();
+			ResultSet rs = st.executeQuery(sql);
+			
+			if(roomType.equals("Lecture Hall")) {
+				list.add("Select a Lecture Hall on here");
+			}
+			if(roomType.equals("Laboratory")) {
+				list.add("Select a Laboratory on here");
+			}
+			
+			while(rs.next()) {
+				
+				if( rs.getString("id").equals("1") || rs.getString("id").equals("2") || rs.getString("id").equals("3")|| rs.getString("id").equals("4") || rs.getString("id").equals("5") 
+						|| rs.getString("id").equals("6") || rs.getString("id").equals("7") || rs.getString("id").equals("8") || rs.getString("id").equals("9") ) {
+					
+					AIID = "0" + rs.getString("id");
+					
+				}else {
+					AIID = rs.getString("id");
+				}
+				
+				singleRoom = "ID"+ AIID + " | " + rs.getString("buildingName") + " | " + rs.getString("roomName") + " | Capacity: " + rs.getString("capacity");
+				list.add(singleRoom);
+			}
+			roomListArray = list.toArray(new String[0]);
+			
+		}catch (Exception e) {
+			
+		}
+
+		
+		return roomListArray;
+		
+		
+		
+	}
+	
+	
+	
+	//insert into sessionRooms table which are selected
+	public boolean insertIntoSessionRoomTable(String sessionId, String locationId) {
+		
+		boolean isSuccess = false;
+		Connection conn = DBConnect.getConnection();
+		try {
+			
+			String sql = "INSERT INTO AssignedSessionRooms (sessionId, locationId)"
+					+ "VALUES ('"+sessionId+"', '"+locationId+"')";
+			
+			Statement st = conn.createStatement();
+			@SuppressWarnings("unused")
+			int rs = st.executeUpdate(sql);
+			
+			st.close();
+			isSuccess = true;
+			
+		}catch(Exception e) {
+			System.out.println(e.getMessage());
+			isSuccess = false;
+		}
+		
+		return isSuccess;
+		
+	}
+	
+	
+	
+	//make the session as a room assigned session
+	public boolean markAsRoomAssigned(String sessionId) {
+		
+		boolean isSuccess = false;
+		
+		Connection conn = DBConnect.getConnection();
+		
+		
+		try {
+			
+			String sql = "UPDATE Sessions SET roomAssignedStatus = 'assigned' WHERE id = '"+sessionId+"' ";
+			
+			Statement st = conn.createStatement();
+			int rs = st.executeUpdate(sql);
+			
+			st.close();
+			conn.close();
+			isSuccess = true;
+			
+		}catch(Exception e) {
+			System.out.println(e.getMessage());
+			isSuccess = false;
+			
+		}	
+		
+		return isSuccess;
+	}
+	
+	
+	
+	
 }
