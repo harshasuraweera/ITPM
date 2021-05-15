1f6083dad434d833525efea432475b845b136ed1
Harsha Manoj Suraweera <harshamanoj912@gmail.com>
2021-05-15 21:55:21 +0530

    timetable generate UI completed

---
+++ ITPM/src/it19208718/GenerateTimetable.java
@@ -0,0 +1,173 @@
+package it19208718;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.EventQueue;
+
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JButton;
+import javax.swing.BorderFactory;
+import java.awt.Font;
+import javax.swing.JComboBox;
+import javax.swing.JLabel;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+@SuppressWarnings("serial")
+public class GenerateTimetable extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					GenerateTimetable frame = new GenerateTimetable();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public GenerateTimetable() {
+		
+		//do these for each and every JFrame
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		setSize(1280, 720);
+		setResizable(false);
+		setLocationRelativeTo(null);
+		setTitle("Forza Timetable Management System");
+		
+		//set Icon to the window
+		ImageIcon img = new ImageIcon(getClass().getClassLoader().getResource("icon.png"));
+		setIconImage(img.getImage());
+		
+		//inner contentPane
+		contentPane = new JPanel();
+		contentPane.setBackground(new Color(153, 204, 255));
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JPanel panel = new JPanel();
+		panel.setBackground(new Color(153, 255, 153));
+		panel.setBounds(0, 0, 1266, 70);
+		contentPane.add(panel);
+		panel.setLayout(null);
+		
+		JLabel label = new JLabel("Lecturer");
+		label.setFont(new Font("Kristen ITC", Font.PLAIN, 18));
+		label.setBounds(24, 92, 165, 46);
+		contentPane.add(label);
+		
+		JButton btnLecturer = new JButton("Lecturer");
+		btnLecturer.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+				label.setText("Lecturer");
+			}
+		});
+		btnLecturer.setFont(new Font("Kristen ITC", Font.PLAIN, 18));
+		btnLecturer.setFocusable(false);
+		btnLecturer.setBorder(BorderFactory.createEmptyBorder(4, 4, 2, 20));
+		btnLecturer.setBounds(26, 10, 150, 50);
+		panel.add(btnLecturer);
+		
+		JButton btnStudent = new JButton("Student Group");
+		btnStudent.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				label.setText("Student Group");
+			}
+		});
+		btnStudent.setFont(new Font("Kristen ITC", Font.PLAIN, 18));
+		btnStudent.setFocusable(false);
+		btnStudent.setBorder(BorderFactory.createEmptyBorder(4, 4, 2, 20));
+		btnStudent.setBounds(228, 10, 199, 50);
+		panel.add(btnStudent);
+		
+		JButton btnLocation = new JButton("Location");
+		btnLocation.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				label.setText("Location");
+			}
+		});
+		btnLocation.setFont(new Font("Kristen ITC", Font.PLAIN, 18));
+		btnLocation.setFocusable(false);
+		btnLocation.setBorder(BorderFactory.createEmptyBorder(4, 4, 2, 20));
+		btnLocation.setBounds(474, 10, 150, 50);
+		panel.add(btnLocation);
+		
+		JButton btnPrint = new JButton("Print");
+		btnPrint.setFont(new Font("Kristen ITC", Font.PLAIN, 18));
+		btnPrint.setFocusable(false);
+		btnPrint.setBorder(BorderFactory.createEmptyBorder(4, 4, 2, 20));
+		btnPrint.setBounds(1086, 10, 150, 50);
+		panel.add(btnPrint);
+		
+		JComboBox<Object> allTheSessionsDropDownList = new JComboBox<Object>(new Object[]{});
+		//allTheSessionsDropDownList.setSelectedIndex(0);
+		allTheSessionsDropDownList.setFont(new Font("Kristen ITC", Font.PLAIN, 18));
+		allTheSessionsDropDownList.setBackground(Color.WHITE);
+		allTheSessionsDropDownList.setBounds(208, 102, 818, 36);
+		contentPane.add(allTheSessionsDropDownList);
+		
+		JButton btnGenerate = new JButton("Generate");
+		btnGenerate.setFont(new Font("Kristen ITC", Font.PLAIN, 18));
+		btnGenerate.setFocusable(false);
+		btnGenerate.setBorder(BorderFactory.createEmptyBorder(4, 4, 2, 20));
+		btnGenerate.setBounds(1080, 94, 150, 50);
+		contentPane.add(btnGenerate);
+		
+		
+		
+		//end default
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+	
+	
+	//
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
